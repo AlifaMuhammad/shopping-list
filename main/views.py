67ee54b058d979d574c7ec6bc8a51781ee13f1bd
alif <alifa.m@ui.ac.id>
2023-09-06 13:23:41 +0700

    menyelesaikan tutorial 2

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def show_main(request):
+    context = {
+        'name': 'Alifa M Hafidz',
+        'class': 'PBP A'
+    }
+
+    return render(request, "main.html", context)
+# Create your views here.
